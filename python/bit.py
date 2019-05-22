9eee9d31320f66959a55936af3894858cc566756
Phil Mansfield <mansfield@uchicago.edu>
2019-05-22 14:09:06 -0500

    Added biy.py I/O helper functions

--- python/bit.py
+++ python/bit.py
@@ -14,3 +14,13 @@ def from_array(arr, bits, length):
     assert(type(arr) == np.ndarray)
     assert(arr.dtype == np.uint8)
     return cy_bit.from_array(arr, np.uint64(bits), np.uint64(length))
+
+def write_array(f, bits, x):
+    if bits == 0: return
+    f.write(array(bits, x).tobytes())
+
+def read_array(f, bits, length):
+    if bits == 0: return np.zeros(length, dtype=np.uint64)
+    buf = np.frombuffer(f.read(array_bytes(bits, length)), dtype=np.uint8)
+    # The extra array conversion here is needed to get rid of a read-only bit
+    return from_array(np.array(buf, dtype=np.uint8), bits, length)
