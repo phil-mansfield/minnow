adf95a4c1295d757b0ec2cc5ee06911e69f82b29
Phil Mansfield <mansfield@uchicago.edu>
2019-05-24 11:11:52 -0500

    Tested Go implementation of minh readers/writers

--- minh/minh.go
+++ minh/minh.go
@@ -30,7 +30,7 @@ type Writer struct {
 }
 
 type Column struct {
-	ColumnType int64
+	Type int64
 	Log int32
 	Low, High, Dx float32
 	Buffer [232]byte
@@ -66,21 +66,22 @@ func (minh *Writer) Block(cols []interface{}) {
 			len(minh.cols), len(cols)))
 	}
 	for i := range cols {
-		colType := minh.cols[i].ColumnType
+		colType := minh.cols[i].Type
 		if err := minnow.TypeMatch(cols[i], colType); err != nil {
-			panic(fmt.Sprintf("Column %d: %s", err.Error()))
+			panic(fmt.Sprintf("Column %d: %s", i, err.Error()))
 		}
 	}
 	
 	N := reflect.ValueOf(cols[0]).Len()
 	minh.blockSizes = append(minh.blockSizes, int64(N))
+	minh.blocks++
 
 	for i := range cols {
 		if Ni := reflect.ValueOf(cols[0]).Len(); N != Ni {
 			panic(fmt.Sprintf("len(cols[%d]) = %d instead of %d", Ni, i, N))
 		}
 
-		colType := minh.cols[i].ColumnType
+		colType := minh.cols[i].Type
 		switch {
 		case colType >= minnow.Int64Group && colType <= minnow.Float32Group:
 			minh.f.FixedSizeGroup(colType, N)
@@ -92,10 +93,22 @@ func (minh *Writer) Block(cols []interface{}) {
 			lim := [2]float32{ minh.cols[i].Low, minh.cols[i].High }
 			x := cols[i].([]float32)
 			minh.buf = expandFloat32(minh.buf, len(x))
-			for i := range x {
-				minh.buf[i] = float32(math.Log10(float64(x[i])))
+			for j := range x {
+				minh.buf[j] = x[j]
+
+				if minh.cols[i].Log != 0 {
+					minh.buf[j] = float32(math.Log10(float64(minh.buf[j])))
+				}
+				if minh.buf[j] < minh.cols[i].Low {
+					minh.buf[j] = minh.cols[i].Low
+				}
+				if minh.buf[j] >= minh.cols[i].High {
+					minh.buf[j] = math.Nextafter32(
+						minh.cols[i].High, float32(math.Inf(-1)),
+					)
+				}
 			}
-			
+
 			minh.f.FloatGroup(N, lim, minh.cols[i].Dx)
 			minh.f.Data(minh.buf)
 		}
@@ -109,9 +122,15 @@ func (minh *Writer) Close() {
 }
 
 func expandFloat32(buf []float32, N int) []float32 {
-	if cap(buf) <= N { return buf[:N] }
+	if cap(buf) >= N { return buf[:N] }
 	buf = buf[:cap(buf)]
 	return append(buf, make([]float32, N - len(buf))...)
+}
+
+func expandInt64(buf []int64, N int) []int64 {
+	if cap(buf) >= N { return buf[:N] }
+	buf = buf[:cap(buf)]
+	return append(buf, make([]int64, N - len(buf))...)
 }
 
 type Reader struct {
@@ -141,31 +160,32 @@ func Open(fname string) *Reader {
 	byteText := make([]byte, f.HeaderSize(2))
 	cols := make([]Column, f.HeaderSize(3)/int(unsafe.Sizeof(Column{})))
 	i64Blocks := int64(0)
-	i64BlockSizes := make([]int64, f.HeaderSize(5) / 8)
-	
+	i64BlockLengths := make([]int64, f.HeaderSize(5) / 8)
+
 	f.Header(1, byteNames)
 	f.Header(2, byteText)
 	f.Header(3, cols)
 	f.Header(4, &i64Blocks)
-	f.Header(5, i64BlockSizes)
+	f.Header(5, i64BlockLengths)
 	
 	minh := &Reader{
+		f: f,
 		Names: strings.Split(string(byteNames), "$"),
 		Text: string(byteText),
 		Columns: cols,
 		Blocks: int(i64Blocks),
-		BlockLengths: make([]int, len(cols)),
+		BlockLengths: make([]int, len(i64BlockLengths)),
 	}
 
-	for i := 0; i < len(cols); i++ {
-		minh.BlockLengths[i] = int(i64BlockSizes[i])
-		minh.Length += int(i64BlockSizes[i])
+	for i := 0; i < len(i64BlockLengths); i++ {
+		minh.BlockLengths[i] = int(i64BlockLengths[i])
+		minh.Length += int(i64BlockLengths[i])
 	}
 
 	return minh
 }
 
-func (rd *Reader) ReadInt(names []string) map[string][]int64 {
+func (rd *Reader) Ints(names []string) map[string][]int64 {
 	out := map[string][]int64{ }
 	for _, name := range names { out[name] = make([]int64, rd.Length) }
 
@@ -176,16 +196,16 @@ func (rd *Reader) ReadInt(names []string) map[string][]int64 {
 		end = start + rd.BlockLengths[b]
 		bOut := map[string][]int64{ }
 
-		for _, name := range names { bOut[name] = out[name][start:end]}
-		rd.ReadIntBlock(b, bOut)
+		for _, name := range names { bOut[name] = out[name][start:end] }
+		rd.IntBlock(b, bOut)
 	}
 
 	return out
 }
 
-func (rd *Reader) ReadFloat([]string) map[string][]float32 {
+func (rd *Reader) Floats(names []string) map[string][]float32 {
 	out := map[string][]float32{ }
-	for _, name := range rd.Names { out[name] = make([]float32, rd.Length) }
+	for _, name := range names { out[name] = make([]float32, rd.Length) }
 
 	end := 0
 
@@ -194,36 +214,55 @@ func (rd *Reader) ReadFloat([]string) map[string][]float32 {
 		end = start + rd.BlockLengths[b]
 		bOut := map[string][]float32{ }
 
-		for _, name := range rd.Names { bOut[name] = out[name][start:end] }
-		rd.ReadFloatBlock(b, bOut)
+		for _, name := range names { bOut[name] = out[name][start:end] }
+		rd.FloatBlock(b, bOut)
 	}
 
 	return out
 }
 
-func (rd *Reader) ReadIntBlock(b int, out map[string][]int64) {
+func (rd *Reader) IntBlock(b int, out map[string][]int64) {
 	runtime.GC()
 	for name, arr := range out {
+		arr = expandInt64(arr, rd.BlockLengths[b])
+
 		if len(arr) != rd.BlockLengths[b] {
 			panic(fmt.Sprintf("Reader.BlockLengths[%d] = %d, but " + 
 				"len(out[%s]) = %d", rd.BlockLengths[b], b, name, len(arr)))
 		}
 
-		idx := findName(name, rd.Names) + b*len(rd.Columns)
-		rd.f.Data(idx, out)
+		c := findName(name, rd.Names)
+		idx := c + b*len(rd.Columns)
+		if err := minnow.TypeMatch(arr, rd.Columns[c].Type); err != nil {
+			panic(fmt.Sprintf("Column '%s': %s", name, err.Error()))
+		}
+
+		rd.f.Data(idx, arr)
+
+		out[name] = arr
 	}
 }
 
-func (rd *Reader) ReadFloatBlock(b int, out map[string][]float32) {
+func (rd *Reader) FloatBlock(b int, out map[string][]float32) {
 	runtime.GC()
 	for name, arr := range out {
-		if len(arr) != rd.BlockLengths[b] {
-			panic(fmt.Sprintf("Reader.BlockLengths[%d] = %d, but " + 
-				"len(out[%s]) = %d", rd.BlockLengths[b], b, name, len(arr)))
+		arr = expandFloat32(arr, rd.BlockLengths[b])
+
+		c := findName(name, rd.Names)
+		idx := c + b*len(rd.Columns)
+		if err := minnow.TypeMatch(arr, rd.Columns[c].Type); err != nil {
+			panic(fmt.Sprintf("Column '%s': %s", name, err.Error()))
 		}
 
-		idx := findName(name, rd.Names) + b*len(rd.Columns)
-		rd.f.Data(idx, out)
+		rd.f.Data(idx, arr)
+
+		if rd.Columns[c].Log != 0 {
+			for i := range arr {
+				arr[i] = float32(math.Pow(10, float64(arr[i])))
+			}
+		}
+
+		out[name] = arr
 	}
 }
 
---
+++ minh/minh_test.go
@@ -0,0 +1,156 @@
+package minh
+
+import (
+	"math"
+	"testing"
+
+	"github.com/phil-mansfield/minnow"
+)
+
+func TestReaderWriter(t *testing.T) {
+	fname := "../test_files/reader_writer_minh.test"
+	
+	names := []string{
+		"int64", "float32", "int", "float", "log",
+	}
+
+	text := "Cats are the best. Don't we love them?!@#$%^&*(),.." + 
+		"..[]{};':\"|\\/-=_+`~meow meow meow"
+
+	columns := []Column{
+		Column{Type: minnow.Int64Group},
+		Column{Type: minnow.Float32Group},
+		Column{Type: minnow.IntGroup},
+		Column{Type: minnow.FloatGroup, Low: 100, High: 200, Dx: 1},
+		Column{Type: minnow.FloatGroup, Log: 1, Low: 10, High: 14, Dx: 0.01},
+	}
+
+	block1 := []interface{}{
+		[]int64{100, 200, 300, 400, 500},
+		[]float32{150, 250, 350, 450, 550},
+		[]int64{-30, -35, -25, -10, -20},
+		[]float32{100, 200, 125, 150, 100},
+		[]float32{1e10, 1e11, 1e11, 1e14, 3e13},
+	}
+
+	block2 := []interface{}{
+		[]int64{125, 225, 325},
+		[]float32{1750, 2750, 3750},
+		[]int64{1000, 1000, 1000},
+		[]float32{100, 100, 100},
+		[]float32{1e14, 1e14, 1e14},
+	}
+
+	joinedBlocks := []interface{}{
+		[]int64{100, 200, 300, 400, 500, 125, 225, 325},
+		[]float32{150, 250, 350, 450, 550, 1750, 2750, 3750},
+		[]int64{-30, -35, -25, -10, -20, 1000, 1000, 1000},
+		[]float32{100, 200, 125, 150, 100, 100, 100, 100},
+		[]float32{1e10, 1e11, 1e11, 1e14, 3e13, 1e14, 1e14, 1e14},
+	}
+
+	blocks := [][]interface{}{ block1, block2}
+
+	wr := Create(fname)
+	wr.Header(names, text, columns)
+	for _, block := range blocks { wr.Block(block) }
+	wr.Close()
+
+	floatOut := map[string][]float32{ "float32": nil, "float": nil, "log": nil }
+	intOut := map[string][]int64{ "int64": nil, "int": nil }
+
+	blocks = append(blocks, joinedBlocks)
+
+	rd := Open(fname)
+
+	if !stringsEq(rd.Names, names) || rd.Text != text ||
+		!columnsEq(rd.Columns, columns) || rd.Blocks != 2 ||
+		rd.Length != 8 || !intsEq(rd.BlockLengths, []int{5, 3}) {
+		t.Fatalf("Could not read header: %v %v %v %v %v %v",
+			stringsEq(rd.Names, names),
+			rd.Text == text, columnsEq(rd.Columns, columns), rd.Blocks == 2,
+			rd.Length == 8, intsEq(rd.BlockLengths, []int{5, 3}))
+	}
+
+	for b, block := range blocks {
+		if b < 2 {
+			rd.IntBlock(b, intOut)
+			rd.FloatBlock(b, floatOut)
+		} else {
+			intOut = rd.Ints([]string{"int64", "int"})
+			floatOut = rd.Floats([]string{"float32", "float", "log"})
+		}
+
+		int64Col := block[0].([]int64)
+		float32Col := block[1].([]float32)
+		intCol := block[2].([]int64)
+		floatCol := block[3].([]float32)
+		logCol := block[4].([]float32)
+
+		if !int64sEq(int64Col, intOut["int64"]) {
+			t.Errorf("Block %d) read %v instead of %v for column '%s'.",
+				b, intOut["int64"], int64Col, "int64")
+		}
+		if !float32sEq(float32Col, floatOut["float32"], 1e-6) {
+			t.Errorf("Block %d) read %v instead of %v for column '%s'.",
+				b, floatOut["float32"], float32Col, "float32")
+		}
+		if !int64sEq(intCol, intOut["int"]) {
+			t.Errorf("Block %d) read %v instead of %v for column '%s'.",
+				b, intOut["int"], intCol, "int")
+		}
+		if !float32sEq(floatCol, floatOut["float"], columns[3].Dx) {
+			t.Errorf("Block %d) read %v instead of %v for column '%s'.",
+				b, floatOut["float"], floatCol, "float")
+		}
+		if !log32sEq(logCol, floatOut["log"], columns[4].Dx) {
+			t.Errorf("Block %d) read %v instead of %v for column '%s'.",
+				b, floatOut["log"], logCol, "log")
+		}
+	}
+
+	rd.Close()
+}
+
+
+func stringsEq(x, y []string) bool {
+	if len(x) != len(y) { return false }
+	for i := range x { if x[i] != y[i] { return false } }
+	return true
+}
+
+func intsEq(x, y []int) bool {
+	if len(x) != len(y) { return false }
+	for i := range x { if x[i] != y[i] { return false } }
+	return true
+}
+
+func columnsEq(x, y []Column) bool {
+	if len(x) != len(y) { return false }
+	for i := range x { if x[i] != y[i] { return false } }
+	return true
+}
+
+func int64sEq(x, y []int64) bool {
+	if len(x) != len(y) { return false }
+	for i := range x { if x[i] != y[i] { return false } }
+	return true
+}
+
+func float32sEq(x, y []float32, dx float32) bool {
+	if len(x) != len(y) { return false }
+	for i := range x {
+		if x[i] + dx < y[i] || x[i] - dx > y[i] { return false }
+	}
+	return true
+}
+
+func log32sEq(x, y []float32, dx float32) bool {
+	if len(x) != len(y) { return false }
+	for i := range x {
+		lx := float32(math.Log10(float64(x[i])))
+		ly := float32(math.Log10(float64(y[i])))
+		if lx + dx < ly || lx - dx > ly { return false }
+	}
+	return true
+}
