3a1de3dc3effa6890fa5856e0d8baa73c5af3a4d
Phil Mansfield <mansfield@uchicago.edu>
2019-05-26 16:12:20 -0500

    Initial implementation of text.Reader

---
+++ go/text/parse.go
@@ -0,0 +1,195 @@
+package text
+
+import (
+	"fmt"
+	"strconv"
+	"bytes"
+)
+
+// split splits a byte slice at each separating character. Faster than
+// bytes.Split() because slicing is used instead of allocations and because
+// only one separator is used.
+//
+// Some of the calculations associated with uncommenting are done here for a
+// slight performance boost.
+func split(data []byte, sep, comm byte) (lines [][]byte, nComm int) {
+	n, nComm := 0, 0
+	for _, c := range data {
+		if c == sep { n++ }
+		if c == comm { nComm++ }
+	}
+
+	tokens := make([][]byte, n+1)
+
+	idx := 0
+	for j := 0; j < n; j++ {
+		data = data[idx:]
+		idx = bytes.IndexByte(data, sep)
+		tokens[j] = data[:idx]
+		idx++
+	}
+	tokens[n] = data[idx:]
+
+	return tokens, nComm
+}
+
+// uncomment removes file comments  in the form of "data # comment". Optimized
+// for the common case where comments are rare and at the start of the file.
+func uncomment(lines [][]byte, comm byte, nComm int) [][]byte {
+	if nComm == 0 { return lines }
+
+	for i, line := range lines {
+		commentStart := bytes.IndexByte(line, comm)
+		if commentStart == -1 {
+			continue
+		}
+
+		lines[i] = line[:commentStart]
+
+		n := 1
+		for _, c := range line[commentStart+1:] {
+			if c == comm { n++ }
+		}
+
+		nComm -= n
+		if nComm == 0 { return lines }
+	}
+
+	return lines
+}
+
+// trim removes empty lines.
+func trim(lines [][]byte, sep byte) [][]byte {
+	j := 0
+
+	LineLoop:
+	for i, line := range lines {
+		for _, c := range line {
+			if c != sep {
+				lines[j] = lines[i]
+				j++
+				continue LineLoop
+			}
+		}
+	}
+
+	return lines[:j]
+}
+
+func parseInt64s(lines [][]byte, sep byte, idxs []int, out [][]int64) error {
+	if len(lines) == 0 || len(idxs) == 0 { return nil }
+	buf := make([][]byte, len(bytes.Fields(lines[0])))
+
+	maxCol := -1
+	for _, i := range idxs {
+		if i > maxCol { maxCol = i }
+	}
+
+	if maxCol >= len(buf) {
+		return fmt.Errorf(
+			"Data has %d columns, but column %d was requested.",
+			len(buf), maxCol,
+		)
+	}
+
+	for i, line := range lines {
+		// Break line up into fields/words
+
+		words := fields(line, sep, buf)
+		if len(words) != len(buf) {
+			return fmt.Errorf(
+				"Data on line %d has %d columns, not %d.",
+				i+1, len(words), len(buf),
+			)
+		}
+
+		// Parse strings.
+
+		for j := range idxs {
+			x, err := strconv.Atoi(string(words[idxs[j]]))
+			if err != nil { return err }
+			out[j][i] = int64(x)
+		}
+	}
+
+	return nil
+}
+
+func parseFloat32s(
+	lines [][]byte, sep byte, idxs []int, out [][]float32,
+) error {
+	if len(lines) == 0 || len(idxs) == 0 { return nil }
+	buf := make([][]byte, len(bytes.Fields(lines[0])))
+
+	maxCol := -1
+	for _, i := range idxs {
+		if i > maxCol { maxCol = i }
+	}
+
+	if maxCol >= len(buf) {
+		return fmt.Errorf(
+			"Data has %d columns, but column %d was requested.",
+			len(buf), maxCol,
+		)
+	}
+
+	for i, line := range lines {
+
+		// Break line up into fields/words
+
+		words := fields(line, sep, buf)
+		if len(words) != len(buf) {
+			return fmt.Errorf(
+				"Data on line %d has %d columns, not %d.",
+				i+1, len(words), len(buf),
+			)
+		}
+
+		// Parse strings.
+
+		for j := range idxs {
+			x, err := strconv.ParseFloat(string(words[idxs[j]]), 32)
+			if err != nil { return err }
+			out[j][i] = float32(x)
+		}
+	}
+
+	return nil
+}
+
+// Optimized and buffered analog to the standard library's bytes.FieldsFunc()
+// function.
+func fields(data []byte, sep byte, buf [][]byte) [][]byte {
+	n := 0
+	inField := false
+	for _, c := range data {
+		wasInField := inField
+		inField = sep != c
+		if inField && !wasInField { n++ }
+	}
+
+	na := 0
+	fieldStart := -1
+
+	for i := 0; i < len(data) && na < n; i++ {
+		c := data[i]
+
+		if fieldStart < 0 && c != sep {
+			fieldStart = i
+			continue
+		}
+
+		if fieldStart >= 0 && c == sep {
+			buf[na] = data[fieldStart: i]
+			na++
+			fieldStart = -1
+		}
+	}
+
+	if fieldStart >= 0 {
+		buf[na] = data[fieldStart: len(data)]
+		na++
+	}
+
+	return buf[0:na]
+}
---
+++ go/text/text.go
@@ -0,0 +1,261 @@
+package text
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+type Reader struct {
+	f io.ReadSeeker
+	closer io.Closer
+
+	config ReaderConfig
+
+	blocks int
+	blockStarts []int64
+	blockSizes []int64
+	names []string
+	buf []byte
+}
+
+type ReaderConfig struct {
+	SkipLines int
+	Separator byte
+	Comment   byte
+	MaxBlockSize int64
+	MaxItemSize  int64
+}
+
+var DefaultReaderConfig = ReaderConfig{
+	SkipLines: 0,
+	Separator: byte(' '),
+	Comment: byte('#'),
+	MaxBlockSize:  5 * (1 << 30),
+	MaxItemSize: 100 * (1 << 10),
+}
+
+// Open opens a text catalogue and returns a Reader.
+func Open(fname string, configOpt ...ReaderConfig) *Reader {
+	f, err := os.Open(fname)
+	if err != nil { panic(err.Error()) }
+
+	rd := openFromReader(f, configOpt...)
+
+	rd.closer = f
+	return rd
+}
+
+// openFromReader is the same as Open except it works on a generic ReadSeeker.
+// This function exists to make testing easier.
+func openFromReader(f io.ReadSeeker, configOpt ...ReaderConfig) *Reader {
+	config := DefaultReaderConfig
+	if len(configOpt) >= 1 { config = configOpt[0] }
+
+	if config.MaxBlockSize / 2 < config.MaxItemSize {
+		panic(fmt.Sprintf("config.MaxBlockSize = %d, but " + 
+			"config.MaxLineSize = %d", config.MaxBlockSize,
+			config.MaxItemSize))
+	}
+	
+	rd := &Reader{ f: f, config: config }
+	rd.findBlocks(readerSize(f))
+
+	return rd
+}
+
+// readerSize returns the size of a ReadSeeker
+func readerSize(f io.ReadSeeker) int64 {
+	pos, err := f.Seek(0, 1)
+	if err != nil { panic(err.Error()) }
+	size, err := f.Seek(0, 2)
+	if err != nil { panic(err.Error())  }
+	_, err = f.Seek(pos, 0)
+	if err != nil { panic(err.Error()) }
+
+	return size
+}
+
+// findBlocks sets blocks, blockStarts, and blockSizes. 
+func (rd *Reader) findBlocks(size int64) {
+	rd.f.Seek(0, 0)
+
+	rd.blockStarts = []int64{ }
+	
+	for end := int64(0); end != -1; end = rd.nextBlock(size) {
+		rd.blockStarts = append(rd.blockStarts, end)	
+	}
+
+	rd.blocks = len(rd.blockStarts)
+	rd.blockSizes = make([]int64, rd.blocks)
+	for i := 0; i < rd.blocks - 1; i++ {
+		rd.blockSizes[i] = rd.blockStarts[i+1] - rd.blockStarts[i]
+	}
+
+	rd.blockSizes[rd.blocks - 1] = size - rd.blockStarts[rd.blocks - 1]
+}
+
+// nextBlock returns the index of the start of the next block and seeks it.
+func (rd *Reader) nextBlock(size int64) int64 {
+	curr, err := rd.f.Seek(0, 1)
+	if err != nil { panic(err.Error()) }
+
+	// If there are no more blocks in the file
+	if curr + int64(rd.config.MaxBlockSize) >= size {
+		_, err := rd.f.Seek(0, 2)
+		if err != nil { panic(err.Error()) }
+		return -1
+	}
+
+	// Move to start of search region.
+	searchStart := rd.config.MaxBlockSize - rd.config.MaxItemSize + curr
+	_, err = rd.f.Seek(searchStart, 0)
+	if err != nil { panic(err.Error()) }
+
+	// Read search region.
+	rd.buf = expandByte(rd.buf, int(rd.config.MaxItemSize))
+	_, err = io.ReadFull(rd.f, rd.buf)
+	if err != nil { panic(err.Error()) }
+
+	// Find the next newline.
+	delta := int64(bytes.IndexByte(rd.buf, byte('\n')))
+	if delta == -1 { panic("config.MaxItemSize too small.") }
+	blockEnd := searchStart + delta + 1
+
+	_, err = rd.f.Seek(blockEnd, 0)
+	if err != nil { panic(err.Error()) }
+
+	return blockEnd
+}
+
+// LinesHeader returns the first lines lines of the file. Must be smaller than
+// config.MaxItemSize.
+func (rd *Reader) LineHeader(lines int) string { 
+	hdLines, _ := rd.headerLines()
+	if len(hdLines) <= lines { panic("config.MaxItemSize too small.") }
+	return string(bytes.Join(hdLines[:lines], []byte{'\n'}))
+}
+
+// CommentHeader returns all the lines that start with comments at the beginning
+// of the file. The header must be smaller than config.MaxItemSize.
+func (rd *Reader) CommentHeader() string {
+	hdLines, nComm := rd.headerLines()
+	if len(hdLines) == nComm { panic("config.MaxItemSize too small.") }
+	return string(bytes.Join(hdLines[:nComm], []byte{'\n'}))
+}
+
+// headerLines returns all the lines in the first config.MaxItemSize bytes of
+// the file. The number of comments is also returned.
+func (rd *Reader) headerLines() ([][]byte, int) {
+	_, err := rd.f.Seek(0, 0)
+	if err != nil { panic(err.Error())  }
+
+	bufSize := readerSize(rd.f)
+	if bufSize > rd.config.MaxItemSize { bufSize = rd.config.MaxItemSize }
+
+	rd.buf = expandByte(rd.buf, int(bufSize))
+	_, err = io.ReadFull(rd.f, rd.buf)
+	if err != nil { panic(err.Error()) }
+
+	return split(rd.buf, byte('\n'), rd.config.Comment)
+}
+
+// SetNames sets the names associated with each column in the file.
+func (rd *Reader) SetNames(names []string) { rd.names = names }
+
+// Blocks returns the number of blocks in the file.
+func (rd *Reader) Blocks() int { return rd.blocks }
+
+// Close closes the file.
+func (rd *Reader) Close() { rd.closer.Close() }
+
+// Block reads the columns associate with names in block b into the array of
+// []int64 and []float32 buffers in out. You'll need to intialize each field in
+// out with []int64{} or []float32{} instead of nil. Sorry.
+func (rd *Reader) Block(b int, names []string, out []interface{}) {
+	if rd.names == nil {
+		panic("Must call text.Reader.SetNames() before text.Reader.Blocks()")
+	}
+
+	_, err := rd.f.Seek(int64(rd.blockStarts[b]), 0)
+	if err != nil { panic(err.Error()) }
+
+	rd.buf = expandByte(rd.buf, int(rd.blockSizes[b]))
+	_, err = io.ReadFull(rd.f, rd.buf)
+	if err != nil { panic(err.Error()) }
+
+	lines, nComm := split(rd.buf, byte('\n'), rd.config.Comment)
+	lines = uncomment(lines, byte('\n'), nComm)
+	lines = trim(lines, rd.config.Separator)
+
+	for i := range out { out[i] = expandGeneric(out[i], len(lines)) }
+
+	iIdx, iCols, fIdx, fCols := rd.splitByType(names, out)
+	parseInt64s(lines, rd.config.Separator, iIdx, iCols)
+	parseFloat32s(lines, rd.config.Separator, fIdx, fCols)
+}
+
+// splitByType splits the output slices by type (i.e. []int64 and []float32).
+func (rd *Reader) splitByType(
+	names []string, out []interface{},
+) (iIdx []int, iCols [][]int64, fIdx []int, fCols [][]float32) {
+	for i := range out {
+		idx := rd.nameIndex(names[i])
+		switch col := out[i].(type) {
+		case []int64:
+			iIdx = append(iIdx, idx)
+			iCols = append(iCols, col)
+		case []float32:
+			fIdx = append(fIdx, idx)
+			fCols = append(fCols, col)
+		default:
+			panic(fmt.Sprintf("Type %T can't be used as an output " + 
+				"for Block()", col))
+		}
+	}
+	return iIdx, iCols, fIdx, fCols
+}
+
+// nameIndex returns the index of the given name.
+func (rd *Reader) nameIndex(name string) int {
+	for i := range rd.names {
+		if strings.ToLower(rd.names[i]) == strings.ToLower(name) {
+			return i
+		}
+	}
+	panic(fmt.Sprintf("Name '%s' doesn't match to any columns.", name))
+}
+
+// expandByte sets the lengths of x to n. This may involve appending.
+func expandByte(x []byte, n int) []byte {
+	if cap(x) >= n { return x[:n] }
+	x = x[:cap(x)]
+	return append(x, make([]byte, n - len(x))...)
+}
+
+// expandByte sets the lengths of x to n. This may involve appending.
+func expandFloat32(x []float32, n int) []float32 {
+	if cap(x) >= n { return x[:n] }
+	x = x[:cap(x)]
+	return append(x, make([]float32, n - len(x))...)
+}
+
+// expandByte sets the lengths of x to n. This may involve appending.
+func expandInt64(x []int64, n int) []int64 {
+	if cap(x) >= n { return x[:n] }
+	x = x[:cap(x)]
+	return append(x, make([]int64, n - len(x))...)
+}
+
+// expandGeneric sets the lengths of x to n. This may involve appending.
+func expandGeneric(x interface{}, n int) interface{} {
+	switch col := x.(type) {
+	case []int64: return expandInt64(col, n)
+	case []float32: return expandFloat32(col, n)
+	default:
+		panic(fmt.Sprintf("Type %T can't be used as an output " + 
+			"for Block()", col))
+	}
+}
