8c807c8bb9a2dee87987a50a431c56f9d5b3633c
phil-mansfield <mansfield@uchicago.edu>
2019-05-27 22:08:19 -0500

    more scripts added

---
+++ scripts/name_index/name_index.go
@@ -0,0 +1,65 @@
+package name_index
+
+import (
+	"io/ioutil"
+	"strings"
+	"sort"
+)
+
+type Index struct {
+	orig, standard []string
+}
+
+func (idx *Index) Len() int { return len(idx.orig) }
+func (idx *Index) Less(i, j int) bool { return idx.orig[i] < idx.orig[j] }
+func (idx *Index) Swap(i, j int) {
+	idx.orig[i], idx.orig[j] = idx.orig[j], idx.orig[i]
+	idx.standard[i], idx.standard[j] = idx.standard[j], idx.standard[i]
+}
+
+func Open(fname string) *Index {
+	bText, err := ioutil.ReadFile(fname)
+	if err != nil { panic(err.Error()) }
+	text := string(bText)
+	
+	lines := clean(strings.Split(text, "\n"))
+
+	orig, standard := []string{}, []string{}
+	for i := range lines {
+		tok := clean(strings.Split(lines[i], " "))
+		for j := range tok {
+			orig = append(orig, tok[j])
+			standard = append(standard, tok[0])
+		}
+	}
+
+	idx := &Index{ orig: orig, standard: standard }
+	sort.Sort(idx)
+
+	return idx
+}
+
+func (idx *Index) Standardize(name string) (std string, inIndex bool) {
+	name = strings.ToLower(name)
+	i := sort.SearchStrings(idx.orig, name)
+
+	if i == len(idx.orig) || idx.orig[i] != name {
+		return name, false
+	} else {
+		return idx.standard[i], true
+	}
+}
+
+func clean(tok []string) []string {
+	for i := range tok {
+		tok[i] = strings.Trim(tok[i], " \n\t")
+	}
+
+	out := []string{}
+	for i := range tok {
+		if len(tok[i]) == 0 { continue }
+		out = append(out, tok[i])
+	}
+
+	return out
+}
--- scripts/test_name_index.go
+++ scripts/test_name_index.go
@@ -3,41 +3,34 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
 	"path"
-	"sort"
-	"strings"
 
 	"github.com/phil-mansfield/minnow/go/text"
+	index "github.com/phil-mansfield/minnow/scripts/name_index"
 )
 
 func main() {
+	nameIndex := "name_index/name_index.txt"
 	hlistDir := "/project/surph/rein_dmo/tests/hlists"
+
+	idx := index.Open(nameIndex)
+	
 	simDirInfo, err := ioutil.ReadDir(hlistDir)
 	if err != nil { panic(err.Error()) }
-
 	hlists := make([]string, len(simDirInfo)) 
 	for i := range hlists {
 		hlists[i] = path.Join(
 			hlistDir, simDirInfo[i].Name(), "hlist_1.00000.list",
 		)
 	}
-	
-	names := []string{ }
 
 	for i := range hlists {
-		if _, err := os.Stat(hlists[i]); err != nil { panic(err.Error())  }
-		
-		f := text.OpenRockstar(hlists[i])
-		names = append(names, f.Names()...)
-	}
+		names := text.OpenRockstar(hlists[i]).Names()
 
-	for i := range names {
-		names[i] = strings.ToLower(names[i])
-	}
-	sort.Strings(names)
-
-	for i := 0; i < len(names) - 1; i++ {
-		if names[i] != names[i + 1] { fmt.Println(names[i+1]) }
+		for j := range names  {
+			if _, ok := idx.Standardize(names[j]); !ok {
+				fmt.Println(names[j])
+			}
+		}
 	}
 }
